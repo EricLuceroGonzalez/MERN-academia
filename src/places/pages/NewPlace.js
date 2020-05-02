98411eaa060a57186abbfbae606e3c51e9b8e039
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-05-01 22:51:10 -0500

    Setting validators functions and Blur effect for validation error show

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
-import './NewPlace.css'
+// Import validators from input
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <form className='place-form'>
-  <Input type='text' label='title'></Input>
-  </form>
+  return (
+    <form className="place-form">
+      <Input
+        type="text"
+        label="title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Enter a valid title"
+      ></Input>
+    </form>
+  );
 };
 
 export default NewPlace;
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,19 +1,85 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import "./Input.css";
+import { validate } from "../../util/validators";
+
+// To feed the reducer:
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        //   Copy old state:
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    case "TOUCH":
+      return {
+        ...state,
+        isTouched: true,
+      };
+    default:
+      return state;
+  }
+};
+
+// ?
+// const touchHandler = () => {
+//   dispatch({ type: "TOUCH" });
+// };
 
 const Input = (props) => {
+  // The reducer that receive state and update
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isTouched: false,
+    isValid: false,
+  });
+
+  // change data handler:
+  const changeHandler = (event) => {
+    //   Store the value and update state:
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
+
+  //   Touch handler to throw errors only if touch input:
+  const touchHandler = () => {
+    dispatch({ type: "TOUCH" });
+  };
+
   // Create an form element (input AND textarea):
   const element =
-    props.element === "input" ? ( 
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+    props.element === "input" ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onBlur={touchHandler}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={props.id} drows={props.rows || 3} />
+      <textarea
+        id={props.id}
+        drows={props.rows || 3}
+        onBlur={touchHandler}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     );
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && "form-control--invalid"
+      }`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
     </div>
   );
 };
