37ba7be2c549e642a3267a824f4b6622ddde503b
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-05-16 15:42:08 -0500

    Setting the form-hhok basic

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer } from "react";
+import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
@@ -9,48 +9,12 @@ import {
 } from "../../shared/util/validators";
 import "./PlaceForm.css";
 
-// Receives state and action and return new state
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case "INPUT_CHANGE":
-      let formIsValid = true;
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      // Return new state accodung to the input is changing
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: { value: action.value, isValid: action.isValid },
-        },
-        isValid: formIsValid,
-      };
-    default:
-      return state;
-  }
-};
+// Moved formReducer to hooks/form-hook
 
 const NewPlace = () => {
   // To manage multiple states (changing) as the title input and others inputs changing
-  // INITIAL STATE
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
-      title: {
-        value: "",
-        isValid: false,
-      },
-      description: {
-        value: "",
-        isValid: false,
-      },
-    },
-    isValid: false,
-  });
+  // INITIAL STATE -->> function copied to form-hooks
+
 
   // Funcition
   const inputHandler = useCallback((id, value, isValid) => {
---
+++ src/shared/hooks/form-hook.js
@@ -0,0 +1,48 @@
+import { useCallback, useReducer } from "react";
+
+// Adding the formReducer that was in NewPlace
+// Receives state and action and return new state
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      // Return new state accodung to the input is changing
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useForm = () => {
+  // To manage multiple states (changing) as the title input and others inputs changing
+  // INITIAL STATE
+
+  // Paste it from NewPlace.js
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+};
