c9fb2dbb6d6ff124475ad4503d059c2c2a3df4b0
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-06-04 17:33:34 -0500

    localStorage at browser the token of user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 // import "bootstrap/dist/css/bootstrap.css";
 import {
   BrowserRouter as Router,
@@ -24,13 +24,26 @@ const App = () => {
   const login = useCallback((uid, token) => {
     setToken(token);
     setUserId(uid);
+    // Storing session auth data:
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({ userId: uid, token: token })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem("userData");
   }, []);
 
+  // Check localStorage for token
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (storedData && storedData.token) {
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]);
   // If user logs in ==> change the route:
   let routes;
   if (token) {
